f47bbe91b15bf3ac8c5335207ccd720a06cbf3d0
Arthur <gitmail10@gmail.com>
2026-03-15 17:30:03 +0200

    fix: nuxt

---
+++ client/auth/app/pages/posts/[id].vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="container">
+		<div
+			class="grid grid-cols-1 gap-[12px] md:grid-cols-2 p-[16px] min-h-screen"
+		>
+			<Logo text="MyStocks" />
+		</div>
+	</div>
+</template>
