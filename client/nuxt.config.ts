621539626622778665401f92cd72e2bd2fefaddc
Arthur <gitmail10@gmail.com>
2026-03-17 17:03:30 +0200

    fix: vue error

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	modules: ['@nuxt/ui', '@pinia/nuxt'],
+	build: {
+		transpile: ['vue'],
+	},
 	css: ['./app/assets/css/main.css'],
 	runtimeConfig: {
 		public: {
